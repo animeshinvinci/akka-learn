a4e6a9732057bddf7bc987308b656a24a03e41a1
animesh <animesh@ebctechnologies.com>
2017-06-25 20:07:54 +0530

    updating app for testing

--- src/main/scala/io/akka/learn/TestActor.scala
+++ src/main/scala/io/akka/learn/TestActor.scala
@@ -18,31 +18,32 @@ case object AskNameMessage
 class TestActor extends Actor {
   def receive = {
     case AskNameMessage => // respond to the 'ask' request
-      sender ! "Fred"
+      sender ! "animesh"
     case _ => println("that was unexpected")
   }
 }
 
 
-object AskTest extends App {
+object AskTestApp extends App {
 
   // create the system and actor
-  val system = ActorSystem("AskTestSystem")
+  val system = ActorSystem("AskTestApp")
   val myActor = system.actorOf(Props[TestActor], name = "myActor")
 
-  // (1) this is one way to "ask" another actor for information
+
+  // a- this is one way to "ask" another actor for information
 
   implicit val timeout = Timeout(5 seconds)
   val future = myActor ? AskNameMessage
   val result = Await.result(future, timeout.duration).asInstanceOf[String]
   println(result)
 
-  // (2) a slightly different way to ask another actor for information
+  // b-  a slightly different way to ask another actor for information
   val future2: Future[String] = ask(myActor, AskNameMessage).mapTo[String]
   val result2 = Await.result(future2, 1 second)
   println(result2)
 
- // this is 3rd way of doing it
+ // c- this is 3rd way of doing it
   val future3: Future[String] = (myActor ? AskNameMessage).mapTo[String]
   val result3 = Await.result(future3, 1 second)
   println(result3)
