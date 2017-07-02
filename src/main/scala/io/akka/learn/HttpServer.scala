ab0ff04f090248a8492db2bea49c775fb1f4bb66
animesh <animesh@ebctechnologies.com>
2017-07-02 23:56:01 +0530

    akka http examples

---
+++ src/main/scala/io/akka/learn/HttpServer.scala
@@ -0,0 +1,34 @@
+package io.akka.learn
+
+import scala.concurrent.duration._
+
+import akka.actor.{ActorSystem}
+import akka.util.{Timeout}
+import akka.pattern.ask
+import akka.io.IO
+import akka.http.Http
+import akka.http.model.{HttpRequest,HttpResponse}
+import akka.stream.scaladsl.Flow
+import akka.stream.{FlowMaterializer}
+
+/**
+  * Created by animesh on 7/2/17.
+  * Created by animesh on 7/2/17.
+  */
+object HttpServer {
+  implicit val askTimeout: Timeout = 1000.millis
+
+  def bindServer(port: Int)(handler: (HttpRequest) => HttpResponse)(implicit system: ActorSystem, materializer: FlowMaterializer) {
+    implicit val ec = system.dispatcher
+    val bindingFuture = IO(Http) ? Http.Bind(interface = "localhost", port = port)
+    bindingFuture foreach {
+      case Http.ServerBinding(localAddress, connectionStream) =>
+        Flow(connectionStream).foreach({
+          case Http.IncomingConnection(remoteAddress, requestProducer, responseConsumer) =>
+            println("Accepted new connection from " + remoteAddress)
+            Flow(requestProducer).map(handler).produceTo(responseConsumer)
+        })
+    }
+  }
+
+}
