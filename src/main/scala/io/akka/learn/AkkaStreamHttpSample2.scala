2c08e851c3431dda8aaab4f7c4cbf9909d99f433
animesh <animesh@ebctechnologies.com>
2017-07-11 23:59:11 +0530

    akka stream http

---
+++ src/main/scala/io/akka/learn/AkkaStreamHttpSample2.scala
@@ -0,0 +1,49 @@
+package io.akka.learn
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.ContentTypes.`text/plain(UTF-8)`
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+
+import scala.util.{Failure, Success}
+
+/**
+  * Created by animesh on 7/11/17.
+  */
+object AkkaStreamHttpSample2 extends App {
+
+  implicit val system = ActorSystem()
+  import system.dispatcher
+  implicit val mat = ActorMaterializer()
+
+  val numbers =
+    Source.unfold(0L) { (n) =>
+      val next = n + 1
+      Some((next, next))
+    }.map(n => ByteString(n + "\n"))
+
+  val route =
+    path("numbers") {
+      get {
+        complete(
+          HttpResponse(entity = HttpEntity(`text/plain(UTF-8)`, numbers))
+        )
+      }
+    }
+  val futureBinding = Http().bindAndHandle(route, "127.0.0.1", 8080)
+
+  futureBinding.onComplete {
+    case Success(binding) =>
+      val address = binding.localAddress
+      println(s"Akka HTTP server running at ${address.getHostString}:${address.getPort}")
+
+    case Failure(ex) =>
+      println(s"Failed to bind HTTP server: ${ex.getMessage}")
+      ex.fillInStackTrace()
+
+  }
+}
